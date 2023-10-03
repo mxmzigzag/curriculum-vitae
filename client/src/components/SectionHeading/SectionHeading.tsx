b182be7ced6134756b126e2403d262361580736a
mxmzigzag <qzzik97@gmail.com>
2023-10-03 11:45:47 +0300

    FE: Updated section hedding to handle more than one word

--- client/src/components/SectionHeading/SectionHeading.tsx
+++ client/src/components/SectionHeading/SectionHeading.tsx
@@ -7,7 +7,7 @@ export const SectionHeading: FC<ISectionHeadingProps> = memo(({ text }) => {
       <h2 className="text-4xl font-bold uppercase tracking-widest z-10">
         {text}
       </h2>
-      <span className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 opacity-50 text-8xl text-cBlack uppercase font-bold drop-shadow-[0_0px_1.2px_rgba(223,178,82,0.9)]">
+      <span className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 opacity-50 text-8xl text-cBlack uppercase font-bold whitespace-nowrap drop-shadow-[0_0px_1.2px_rgba(223,178,82,0.9)]">
         {text}
       </span>
     </div>
